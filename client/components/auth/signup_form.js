f419ff43ec612055659ea4682d88f01131dd0aa4
Manoj <manoj.delhi24@gmail.com>
2017-06-29 16:49:01 +0530

    static html form added through react component

---
+++ client/components/auth/signup_form.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class SignupForm extends React.Component {
+
+  render() {
+    return(
+      <form >
+        <h1>Join our community! </h1>
+        <div className="form-group">
+          <label className="control-label">Username</label>
+          <input type="text" name="username" className="form-control" />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-primary btn-lg">
+            Sign Up
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default SignupForm;
--- client/components/auth/signup_page.js
+++ client/components/auth/signup_page.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import SignupForm from './signup_form'
 
 class SignupPage extends React.Component {
 
   render() {
     return(
-      <h3> Signup Page</h3>
+      <div className="row">
+        <div className="col-md-4 col-md-offset-4">
+          <SignupForm />
+        </div>
+      </div>
     );
   }
 }
